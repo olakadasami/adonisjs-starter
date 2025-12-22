bbf3d26b3a22d017db10cff0398eb1fe27164320
olakada samuel <olakadasamuel@gmail.com>
2025-12-22 22:51:42 +0100

    chore: handle error responses app wide

--- app/exceptions/handler.ts
+++ app/exceptions/handler.ts
@@ -1,5 +1,9 @@
 import app from '@adonisjs/core/services/app'
 import { HttpContext, ExceptionHandler } from '@adonisjs/core/http'
+import { errors as mailErrors } from '@adonisjs/mail'
+import { errors as lucidErrors } from '@adonisjs/lucid'
+import { errors } from '@adonisjs/core'
+import { errors as bouncerErrors } from '@adonisjs/bouncer'
 
 export default class HttpExceptionHandler extends ExceptionHandler {
   /**
@@ -13,6 +17,39 @@ export default class HttpExceptionHandler extends ExceptionHandler {
    * response to the client
    */
   async handle(error: unknown, ctx: HttpContext) {
+    if (error instanceof mailErrors.E_MAIL_TRANSPORT_ERROR) {
+      console.log(error.cause)
+      return ctx.response.badRequest({
+        status: 'Not found',
+        message: `${error.cause || error.message} ${error.message}`,
+        statusCode: 404,
+      })
+    }
+
+    if (error instanceof lucidErrors.E_ROW_NOT_FOUND) {
+      return ctx.response.notFound({
+        status: 'Not found',
+        message: `${error.model?.name || 'Row'} not found`,
+        statusCode: 404,
+      })
+    }
+    if (error instanceof errors.E_ROUTE_NOT_FOUND) {
+      // handle error
+      return ctx.response.notFound({
+        status: 'Not Found',
+        message: 'Route/Method does not exist',
+        statusCode: 404,
+      })
+    }
+
+    if (error instanceof bouncerErrors.E_AUTHORIZATION_FAILURE) {
+      return ctx.response.status(403).json({
+        status: 'Forbidden',
+        statusCode: 403,
+        message: 'Access denied: You do not have permission to access this resource.',
+      })
+    }
+
     return super.handle(error, ctx)
   }
 
